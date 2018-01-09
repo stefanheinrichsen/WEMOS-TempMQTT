dee2c91ed8935e39ae17de4d8a6d049ea5d9ee60
Stefan Heinrichsen <github@stefan.heinrichsen.net>
2018-01-09 16:31:18 +0100

    done: MQTT publish, MQTT receive (for location value), sending with location as MQTT topic and fallback to MAC address

--- WEMOS-TempMQTT.ino
+++ WEMOS-TempMQTT.ino
@@ -1,17 +1,18 @@
 #include "config.h"
+
+#include <DHT.h>
 #include <DHT_U.h>
 #include <Adafruit_Sensor.h>
 #include <ESP8266WiFi.h>
-#include <MQTTClient.h>    // MQTT Lib: https://github.com/256dpi/arduino-mqtt
-//#include <FS.h>
+#include <MQTTClient.h>          // MQTT Lib: https://github.com/256dpi/arduino-mqtt
+#include <ArduinoOTA.h>
+#include <FS.h>
 
-// Define Pin for Sensor - I use GPIO2, wich is D4 on WEMOS
-#define DHTPIN 2
+// ToDo: OTA and allow topic setting via MQTT to MAC
 
-// Uncomment whatever type you're using!
-//#define DHTTYPE DHT11     // DHT 11 
+// Define Pin for Sensor - I use GPIO0, wich is D3 on WEMOS
+#define DHTPIN 0
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
 // init DHT; 3rd parameter = 16 works for ESP8266@80MHz
 // FixMe: What is the third parameter??
@@ -23,100 +24,175 @@ MQTTClient mqttClient;
 // Enable ADC to measure Vcc
 ADC_MODE(ADC_VCC);
 
-int lastMillis;
 // Generally, you should use "unsigned long" for variables that hold time
-const long interval = 2000;              // interval at which to read sensor
-unsigned long previousMillis = 0;        // will store last temp was read
+unsigned long previousMillisDHT  = 0;        // will store last temp was read
+unsigned long previousMillisMQTT = 0;
 
+// Char buffer for converting floats to strings
 char buffer[20];
 
+// String for storing WiFi MAC adress
+String mac;
+
+// String for storing the MQTT (parent) topic to send values to
+String mqttTopic(mqttUserTopic);
+
 float hum, temp;  // Values read from sensor
-int vcc;    // Vcc measured by internal ADC; unit should be 1/1024 Volt
+int vcc;          // Vcc measured by internal ADC; unit: 1/1024 Volt
 
-void messageReceived(String &topic, String &payload) {
-  Serial.println("incoming: " + topic + " - " + payload);
-}
-
-void gettemperature() {
-  // Wait at least 2 seconds seconds between measurements.
-  // if the difference between the current time and last time you read
-  // the sensor is bigger than the interval you set, read the sensor
-  // Works better than delay for things happening elsewhere also
-  unsigned long currentMillis = millis();
- 
-  if(currentMillis - previousMillis >= interval) {
-    // save the last time you read the sensor 
-    previousMillis = currentMillis;   
- 
-    // Reading temperature for humidity takes about 250 milliseconds!
-    // Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
-    hum  = dht.readHumidity();        // Read humidity (percent)
-    temp = dht.readTemperature();     // Read temperature as Celsius ...(true)=Fahrenheit
-    // Check if any reads failed and exit early (to try again).
-    if (isnan(hum) || isnan(temp)) {
-      return;
+void messageReceivedMQTT(String &topic, String &payload) {
+  Serial.println("MQTT incoming topic: " + topic + " - Value: " + payload);
+  
+  if(topic.indexOf("location") > 0) {
+    mqttTopic=payload;
+    
+    File configFile = SPIFFS.open("/mqtt_location.txt", "w");
+    if (!configFile) { 
+      Serial.println("Failed to open mqtt_location.txt for writing new location!");
+    } else {
+      Serial.print("Saving new MQTT location to mqtt_location.txt: "); Serial.println(payload);
+      configFile.println(payload);
+      configFile.close();
     }
   }
+}
+
+void getTempHum() {
+  unsigned long currentMillis = millis();
+
+  // Make sure we only read new values if enough time has passed
+  while(currentMillis - previousMillisDHT < dhtInterval) {
+    currentMillis = millis();
+  }
+  
+  // Reading temperature for humidity takes about 250 milliseconds!
+  do {
+    hum  = dht.readHumidity();        // Read humidity (percent)
+    temp = dht.readTemperature(false);// Read temperature as Celsius ...(true)=Fahrenheit
+  } while(isnan(hum) || isnan(temp)); // Check if any reads failed and try again
+
+  // After reading new values we store the time for the next round
+  previousMillisDHT = millis();  
+}
+
+void connectWiFi() {
+  String hostname(HOSTNAME);
+
+  Serial.println("--- WiFi Setup Start ---");
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  WiFi.printDiag(Serial);
+
+  mac = WiFi.macAddress(); 
+  mac.replace(":", "");
+  Serial.print("MAC: "); Serial.println(mac);
+  
+  hostname += mac;
+  Serial.print("\nSetting Hostname to: "); Serial.println(hostname);
+  WiFi.hostname(hostname);
+  
+  WiFi.begin(wifi_ssid, wifi_pass);
+  Serial.print("Connecting wifi...");
+
+  while (WiFi.status() != WL_CONNECTED) {
+     Serial.print(".");
+     delay(250);
+  }
+  Serial.print("\nWiFi connected! IP address: "); Serial.println(WiFi.localIP());
+  Serial.println("--- WiFi Setup End   ---");
+}
+
+void connectMQTT() {
+  // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported by Arduino.
+  // You need to set the IP address directly.
+  String mqttID = String("ESP_"+mac);
+  
+  Serial.println("\n--- MQTT Setup Start ---");
+  Serial.println("\nconnecting mqtt...");
+  
+  mqttClient.begin(mqttServer, wifiClient);
+  mqttClient.onMessage(messageReceivedMQTT);
+  Serial.print("Connection to MQTT server with client ID: "); Serial.println(mqttID.c_str());
+  while (!mqttClient.connect(mqttID.c_str())) {
+     Serial.print(".");
+     delay(250);
+   }
+   Serial.println("\nMQTT connected!");
+   Serial.println("\n--- MQTT Setup End   ---");
 }
 
 void setup() {
-
-    // Short delay after boot... seems needed sometimes
-    for(uint8_t t = 4; t > 0; t--) {
-        delay(1000);
-    }
-
   Serial.begin(115200);
+  delay(100);
   
-  Serial.println("\n\nStarting wifi\n");
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
-  Serial.print("Connecting wifi...");
-  
-  while (WiFi.status() != WL_CONNECTED) {
-     Serial.print(".");
-     delay(1000);
-  }
-  Serial.print("WiFi connected! IP address: "); Serial.println(WiFi.localIP());
-
-
-  Serial.print("\nconnecting mqtt...");
+  Serial.println("\n\nStarting up\n============\n");
+  Serial.print("\nChip ID: 0x");  Serial.println(ESP.getChipId(), HEX);
  
-  // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported by Arduino.
-  // You need to set the IP address directly.
-  mqttClient.begin(mqttServer, wifiClient);
-  mqttClient.onMessage(messageReceived);
+  connectWiFi();
+  connectMQTT();
 
-  while (!mqttClient.connect("arduino", "try", "try")) {
-     Serial.print(".");
-     delay(1000);
-   }
+  Serial.println("\n\n--- Filesystem and MQTT-Sub Start ---");
+  // Setup filesystem
+  if(!SPIFFS.begin()) { Serial.println("\n!! Error during setup Filesystem! Will use default values for sending MQTT Topic"); }
 
-  Serial.println("\nconnected!");
+  if(mqttTopic.length()==0) {
+    Serial.println("No MQTT User Topic existing. Trying to load from config, fallback to MAC Address");
+    mqttTopic = String(mac);
+    
+    File configFile = SPIFFS.open("/mqtt_location.txt", "r");
+    if (!configFile) { Serial.println("Failed to open mqtt_location.txt. Using MAC as MQTT topic."); }
+    else { 
+       mqttTopic = configFile.readString();
+       Serial.print("MQTT Topic read from mqtt_location.txt: "); Serial.println(mqttTopic);
+       configFile.close();
+       mqttTopic.trim();
+    }
+  } else { 
+    Serial.println("MQTT User Topic provided during compile time. Overriding other values.");
+  }
+  Serial.println("--- MQTT Sub/Pub Summary ---");
+  Serial.print("MQTT publish base topic for this device: "); Serial.println(mqttTopic);
+  Serial.print("MQTT subscription topic for this device: "); Serial.println(mac+"/#");
+  mqttClient.subscribe(mac + "/#");
+  Serial.println("\n--- Filesystem and MQTT-Sub End   ---");
+  
+  // Start OTA server.
+  ArduinoOTA.setHostname((const char *)((WiFi.hostname()).c_str()));
+  ArduinoOTA.begin();
 
-  // mqttClient.subscribe("/hello");
-  // client.unsubscribe("/hello");
+  Serial.println("\n\nEND OF SETUP - Start of normal operation\n========================================\n\n");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // First some handles to call for background tasks
   mqttClient.loop();
+  ArduinoOTA.handle();
 
-  if (!mqttClient.connected()) {
-      // Probelm with mqtt
-      Serial.println("\nConnection to MQTT broker lost.");
-  }
+  // publish a message roughly after "measureDelay" ms
+  if (millis() - previousMillisMQTT > measureDelay) {
+     
+    previousMillisMQTT = millis();
+    getTempHum();
+    vcc=ESP.getVcc();
 
-  gettemperature();
-  vcc=ESP.getVcc();
+    // Check if we are still connected to the MQTT server. If not: reconnect
+    if (!mqttClient.connected()) {                // Problem with MQTT server
+      Serial.println("\nConnection to MQTT broker lost. Reconnecting.\n");
+      
+      if (WiFi.status() != WL_CONNECTED) connectWiFi();
+      connectMQTT();
+      Serial.println("Connection to MQTT restored.");
+    }
 
-  // publish a message roughly every five seconds.
-  if (millis() - lastMillis > 5000) {
-    lastMillis = millis();
-    Serial.println("Sending values via MQTT!");
-    dtostrf(temp, 5, 1, buffer); mqttClient.publish("nordzimmer/temp", buffer);
-    dtostrf( hum, 5, 1, buffer); mqttClient.publish("nordzimmer/hum" , buffer);
-    sprintf(buffer, "%i", vcc);  mqttClient.publish("nordzimmer/vcc" , buffer);
-    Serial.println(temp); Serial.println(buffer); //Serial.println(hum); Serial.println(vcc); 
+    Serial.println("\nSending values via MQTT.");
+    
+    dtostrf(temp, 5, 1, buffer); mqttClient.publish((mqttTopic+"/temp").c_str(), buffer);
+    dtostrf( hum, 5, 1, buffer); mqttClient.publish((mqttTopic+"/hum").c_str() , buffer);
+    sprintf(buffer, "%i", vcc);  mqttClient.publish((mqttTopic+"/vcc").c_str() , buffer);
+    
+    Serial.print("temp: "); Serial.println(temp); 
+    Serial.print("hum: ");  Serial.println(hum);
+    Serial.print("vcc: ");  Serial.println(vcc); 
   }
 }
 
